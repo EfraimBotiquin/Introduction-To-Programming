f88e34e424e4417d1aeac96c1d03870021dcfb23
Efraim <125047379+EfraimBotiquin@users.noreply.github.com>
2025-10-31 22:01:03 +0400

    Delete IntroToPython directory

--- IntroToPython/03-Biography.py
+++
@@ -1,8 +0,0 @@
-
-bio = {
-    "name": "Efraim Botiquin",
-    "hometown": "Albay",
-    "age": 19
-}
-
-print(bio["name"], bio["hometown"], bio["age"], sep="\n")
--- IntroToPython/08-SimpleSearch.py
+++
@@ -1,8 +0,0 @@
-
-names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
-search = input("Enter a name to search for: ").strip()
-
-if search in names:
-    print(f"{search} was found in the list!")
-else:
-    print(f"{search} was not found.")
